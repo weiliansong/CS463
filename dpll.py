aee956d0a9e0e89dadf2696e576993956192a665
Weilian Song <weilian.song@uky.edu>
2017-10-18 09:24:35 -0400

    Worked on DPLL

--- dpll.py
+++ dpll.py
@@ -5,9 +5,9 @@ from util import random_book, bool_eval
 
 def solve(n_vars, clauses):
   book = random_book()
-  return dpll(n_vars, np.copy(clauses), book.copy())
+  return helper(n_vars, clauses, book)
 
-def dpll(n_vars, clauses, book):
+def helper(n_vars, clauses, book):
 
   clause_eval = bool_eval(clauses, book)
   fitness = np.sum(clause_eval)
@@ -18,29 +18,34 @@ def dpll(n_vars, clauses, book):
   vars_to_delete = []
   clauses_to_delete = []
 
-  first, second = np.unique(np.concatenate(clauses), return_count=True)
-  var_counts = dict(zip(first, second))
+  variables, counts = np.unique(np.concatenate(clauses), return_count=True)
+  var_counts = dict(zip(variables, counts))
 
   for var in range(1, n_vars+1):
-    if len(var_counts[var]) == 1 and len(var_counts[-var]) == 0:
+    if len(var_counts[var]) and len(var_counts[-var]) == 0:
       book[var] = 1
       book[-var] = 0
       vars_to_delete.append(var)
 
-    elif len(var_counts[var]) == 0 and len(var_counts[-var]) == 1:
+    elif len(var_counts[var]) == 0 and len(var_counts[-var]):
       book[var] = 0
       book[-var] = 1
       vars_to_delete.append(-var)
+
+    else:
+      continue
 
   for idx, clause in enumerate(clauses):
     if len(clause) == 0:
       return False
     
+    # Check for unit clause
     elif len(clause) == 1:
       var = clause[0]
       book[var] = 1
       book[-var] = 0
       clauses_to_delete.append(idx)
+      continue
 
     for var in vars_to_delete:
       if var in clause:
@@ -48,3 +53,27 @@ def dpll(n_vars, clauses, book):
 
   # Delete all the single-variable clause
   clauses = np.delete(clauses, things_to_delete) 
+
+  # Finished with pruning, picking one random variable to set true
+  lucky_var = np.choice(np.unique(np.concatenate(clauses)))
+
+  true_clauses = []
+  false_clauses = []
+
+  for clause in clauses:
+    if lucky_var in clause and -lucky_var not in clause:
+      clause = np.delete(clause, np.where(clause==lucky_var))
+      false_clauses.append(clause)
+
+    elif lucky_var not in clause and -lucky_var in clause:
+      clause = np.delete(clause, np.where(clause==-lucky_var))
+      true_clauses.append(clause)
+
+    elif lucky_var in clause and -lucky_var in clause:
+      continue
+    
+    else:
+      true_clauses.append(clause)
+      false_clauses.append(clause)
+
+  return helper(n_vars, true_clauses) or helper(n_vars, false_clauses)
