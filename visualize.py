b317cd724d08ea928f85351fdcf2009e42eeeaf2
Weilian Song <weiliansong@hotmail.com>
2017-10-20 03:23:45 -0400

    Worked on visualization script

--- visualize.py
+++ visualize.py
@@ -1,45 +1,83 @@
+import os
 import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
-walksat_csv = glob.glob('./stats/*.csv')
+def grab_data(csv_files):
+  data = {}
 
-walksat_data = {}
+  for csv_f in csv_files:
+    sat_id = csv_f.strip().split('/')[-1]
+    sat_id = sat_id.split('.')[0]
+    sat_id = int(sat_id.split('_')[-1])
 
-for csv_f in walksat_csv:
-  sat_id = csv_f.strip().split('/')[-1]
-  sat_id = sat_id.split('.')[0]
-  sat_id = int(sat_id.split('_')[-1])
+    f = open(csv_f, 'r')
+    f_lines = f.readlines()
+    f.close()
 
-  f = open(csv_f, 'r')
-  f_lines = f.readlines()
-  f.close()
+    if not f_lines:
+      continue
 
-  if not f_lines:
-    continue
+    fitnesses = []
+    times = []
 
-  fitnesses = []
-  times = []
+    for line in f_lines[1:]:
+      fitness, time = line.strip().split(',')
 
-  for line in f_lines[1:]:
-    fitness, time = line.strip().split(',')
+      fitnesses.append(int(fitness))
+      times.append(float(time))
 
-    fitnesses.append(int(fitness))
-    times.append(float(time))
+    data[sat_id] = {}
+    data[sat_id]['fitness'] = max(fitnesses)
+    data[sat_id]['runtime'] = np.average(times)
 
-  walksat_data[sat_id] = {}
-  walksat_data[sat_id]['fitness'] = max(fitnesses)
-  walksat_data[sat_id]['runtime'] = np.average(times)
+  return data
 
-x = np.array(walksat_data.keys())
-y = np.array([walksat_data[x_i]['runtime'] for x_i in x])
+def plot(labels, values, plt_title, y_label):
+  assert len(labels) == len(values)
 
-sort_idx = np.argsort(y)[::-1]
-labels = x[sort_idx][:100]
-y = y[sort_idx][:100]
+  split_labels = np.array_split(labels, 3)
+  split_values = np.array_split(values, 3)
 
-plt.plot(range(len(y)), y)
-plt.xticks(range(len(y)), labels, rotation=90)
-plt.subplots_adjust(bottom=0.15)
-plt.tight_layout()
-plt.show()
+  plot_datas = zip(split_labels, split_values)
+
+  for plot_idx, (_labels, _values) in enumerate(plot_datas):
+    sort_idx = np.argsort(_values)[::-1]
+    _labels = _labels[sort_idx]
+    _values = _values[sort_idx]
+
+    plt.plot(range(len(_values)), _values)
+    plt.xticks(range(len(_labels)), _labels, rotation=90)
+    plt.xlabel('Algorithm ID')
+    plt.ylabel(y_label)
+    plt.tight_layout()
+    plt.title('%s %d' % (plt_title, plot_idx))
+    plt.savefig('./visualizations/%s_%d.png' % (plt_title, plot_idx))
+
+walksat_data = grab_data(glob.glob('./stats_WalkSAT/100_*.csv'))
+genetic_data = grab_data(glob.glob('./stats_Genetic/100_*.csv'))
+dpll_data    = grab_data(glob.glob('./stats_DPLL/100_*.csv'))
+
+if not os.path.exists('./visualizations'):
+  os.mkdir('./visualizations')
+
+# WalkSAT plotting
+labels = np.array(walksat_data.keys())
+runtimes = np.array([walksat_data[label]['runtime'] for label in labels])
+fitnesses = np.array([walksat_data[label]['fitness'] for label in labels])
+plot(labels, runtimes, 'WalkSAT_Runtime', 'Runtime (s)')
+plot(labels, fitnesses, 'WalkSAT_Fitnesses', 'Fitness (# clauses)')
+
+# Genetic plotting
+labels = np.array(genetic_data.keys())
+runtimes = np.array([genetic_data[label]['runtime'] for label in labels])
+fitnesses = np.array([genetic_data[label]['fitness'] for label in labels])
+plot(labels, runtimes, 'Genetic_Runtime', 'Runtime (s)')
+plot(labels, fitnesses, 'Genetic_Fitnesses', 'Fitness (# clauses)')
+
+# DPLL plotting
+labels = np.array(dpll_data.keys())
+runtimes = np.array([dpll_data[label]['runtime'] for label in labels])
+fitnesses = np.array([dpll_data[label]['fitness'] for label in labels])
+plot(labels, runtimes, 'DPLL_Runtime', 'Runtime (s)')
+plot(labels, fitnesses, 'DPLL_Fitnesses', 'Fitness (# clauses)')
