8e932cdb3d2e927d05bd8cfaf0fbfeca73402b1e
Weilian Song <weilian.song@uky.edu>
2017-10-31 17:06:46 -0400

    Working on family DB

---
+++ database.pl
@@ -0,0 +1,26 @@
+man(aaron).
+man(captain).
+man(eagle).
+man(goodman).
+man(ian).
+man(konan).
+man(munich).
+man(norway).
+man(olaf).
+
+woman(becky).
+woman(diana).
+woman(french).
+woman(helen).
+woman(jane).
+woman(lynn).
+woman(queen).
+woman(pam).
+
+/* First row */
+child(captain, aaron).
+child(captain, becky).
+child(french, aaron).
+child(french, becky).
+child(goodman, aaron).
+child(goodman, becky).
