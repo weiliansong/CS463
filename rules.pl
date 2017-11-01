b5ee6b925e9a6a7b6274f6cae22f38334345b09f
Weilian Song <weilian.song@uky.edu>
2017-11-01 07:32:16 -0400

    A few rules added

--- rules.pl
+++ rules.pl
@@ -1,1 +1,14 @@
 parent(P,C) :- child(C,P).
+
+married(A,B) :- parent(A,C), parent(B,C), A \== B.
+
+sibling(A,B) :- parent(C,A), parent(C,B), A \== B.
+
+% http://www.cs.utexas.edu/~cannata/cs345/Class%20Notes/12%20prolog_intro.pdf
+grandparent(A,B) :- parent(A,C), parent(C,B).
+
+% https://stackoverflow.com/questions/30497704/prolog-general-rule-for-finding-cousins-etc
+ancestor(A,B,L) :-
+  parent(A,C),
+  ancestor(C,B,L),
+  L is L + 1.
